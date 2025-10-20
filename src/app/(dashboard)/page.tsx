c1e1cb85b5fea0dc6af8bcb66d074a4fdb351950
Neil <morrison.neil.v@gmail.com>
2025-10-20 15:49:25 -0400

    added dashboard copy

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,12 +1,12 @@
+import { Text } from "@/components/ui/text";
+
 export default function Page() {
   return (
-    <>
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-      </div>
-      <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-    </>
+    <div>
+      <Text variant="h1">Dashboard</Text>
+      <Text dimmed className="text-sm">
+        An overview of all your activity on Big Lifts
+      </Text>
+    </div>
   );
 }
